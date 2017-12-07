b43f5c6b87264f7465a3b26b9e6145cd0589ecdb
louis cochinho <louis.cochinho@hotmail.fr>
2017-12-07 20:46:15 +0100

    back-end first commit

---
+++ server/app/routes/note_routes.js
@@ -0,0 +1,73 @@
+// routes/note_routes.js
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function (app, db) {
+    // ***** GET ***** //
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    // ***** POST ***** //
+    app.post('/notes', (req, res) => {
+        const note = {
+            text: req.body.body,
+            title: req.body.title
+        };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    // ***** DELETE ***** //
+    app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = {
+            '_id': new ObjectID(id)
+        };
+        db.collection('notes').remove(details, (err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
+            } else {
+                res.send('Note ' + id + ' deleted!');
+            }
+        });
+    });
+
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        // Problème si on veut uniquement modifier un attribut
+        if(req.body.body !== undefined && req.body.title !== undefined){
+            const note = { text: req.body.body, title: req.body.title };
+            db.collection('notes').update(details, note, (err, result) => {
+              if (err) {
+                  res.send({'error':'An error has occurred'});
+              } else {
+                  res.send(note);
+              } 
+            });
+        }else{
+            res.send({'error':'An error has occurred'})
+        }
+      });
+};
---
+++ server/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://user_test:pwd_test@ds131546.mlab.com:31546/nodetest"
+  };
