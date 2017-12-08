b2651a683f676773e5d0777e87f3594684bf2c3d
Shloumpf <code.shloumpf@gmail.com>
2017-12-08 03:12:31 +0100

    Testing

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,11 +2,13 @@
 const noteRoutes = require('./note_routes');
 const userApi = require('./user_api');
 const eventApi = require('./event_api');
+// const userEventApi = require('./userevent_api');
 const categoryApi = require('./category_api');
 module.exports = function(app, db) {
   noteRoutes(app, db);
   userApi(app,db);
   eventApi(app,db);
+  // userEventApi(app,db);
   categoryApi(app,db);
   // Other route groups could go here, in the future
 };
