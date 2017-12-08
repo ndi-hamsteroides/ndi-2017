dec750adda8b59c0ce943b8f3c2a7c283b54bf8e
Shloumpf <code.shloumpf@gmail.com>
2017-12-08 05:44:04 +0100

    API UserEvent

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,7 +2,7 @@
 const noteRoutes = require('./note_routes');
 const userApi = require('./user_api');
 const eventApi = require('./event_api');
-// const userEventApi = require('./userevent_api');
+const userEventApi = require('./userevent_api');
 const categoryApi = require('./category_api');
 const infoApi = require('./info_api');
 const infocategoryApi = require('./infocategory_api');
@@ -11,7 +11,7 @@ module.exports = function(app, db) {
   noteRoutes(app, db);
   userApi(app,db);
   eventApi(app,db);
-  // userEventApi(app,db);
+  userEventApi(app,db);
   categoryApi(app,db);
   infoApi(app,db);
   infocategoryApi(app,db);
--- server/models/userevent_model.js
+++ server/models/userevent_model.js
@@ -1,6 +1,7 @@
 'use strict';
 class UserEvent {
-	constructor(id_user,id_event){
+	constructor(_id, id_user,id_event){
+		this._id = _id;
 		this.id_user = id_user;
 		this.id_event = id_event;
 	}
