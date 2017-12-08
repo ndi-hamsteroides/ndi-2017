9f8f0fc17822964d57b60fee31711c8d2cf3f4db
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2017-12-08 05:53:09 +0100

    Reaction test ok
    Subversion to crimestop thoughtcrime and unperson Emmanuel Gitstein

--- webapp/src/app/drive-test/drive-test.component.ts
+++ webapp/src/app/drive-test/drive-test.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class DriveTestComponent implements OnInit {
 
   private start: boolean;
-  private end : boolean;
+  private end: boolean;
+  private wokeup: boolean;
+  private timeout: number;
+  private start_time: any;
+  private end_time: any;
+  private time_taken: any;
+  private ok: boolean;
 
   constructor() { }
 
@@ -18,11 +24,22 @@ export class DriveTestComponent implements OnInit {
   }
 
   doStart() {
+    this.timeout = Math.floor((Math.random() * 2000)) + 200;
     this.start = true;
+    setTimeout(() => {
+      this.wokeup = true;
+      this.start_time = new Date();
+    }, this.timeout);
   }
 
-  doEnd() {
+  doPress() {
     this.end = true;
+    this.end_time = new Date();
+    this.time_taken = this.end_time - this.start_time;
+    this.ok = this.time_taken <= 650;
   }
 
+  private changeImage() {
+    this.wokeup = true;
+  }
 }
