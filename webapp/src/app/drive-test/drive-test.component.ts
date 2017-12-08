4c91666f7ba739a439ad830963105dff73e024d3
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2017-12-08 04:12:21 +0100

    Starting the reflex test

---
+++ webapp/src/app/drive-test/drive-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drive-test',
+  templateUrl: './drive-test.component.html',
+  styleUrls: ['./drive-test.component.css']
+})
+export class DriveTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
